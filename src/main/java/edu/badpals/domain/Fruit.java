7ec707b629f5ed293e62f93166c5618cad86e5d2
bestiadivina147 <diegopc147@gmail.com>
2024-06-11 13:24:15 +0200

    feat(ServiceFruit.java): add add(Fruit fruit) to ServiceFruit.java

--- src/main/java/edu/badpals/domain/Fruit.java
+++ src/main/java/edu/badpals/domain/Fruit.java
@@ -3,16 +3,11 @@ package edu.badpals.domain;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Entity
--- src/main/java/edu/badpals/domain/ServiceFruit.java
+++ src/main/java/edu/badpals/domain/ServiceFruit.java
@@ -1,5 +1,6 @@
 package edu.badpals.domain;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,10 +9,23 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.NoArgsConstructor;
 
-@ApplicationScoped @NoArgsConstructor
+@ApplicationScoped
+@NoArgsConstructor
 public class ServiceFruit {
     public Set<Fruit> list() {
         Stream<Fruit> fruits = Fruit.streamAll();
         return fruits.collect(Collectors.toSet());
+    }
+
+    public void add(Fruit fruit) {
+
+        Optional<Farmer> supplier = Farmer.find("name", fruit.farmer.name).firstResultOptional();
+        if (supplier.isPresent()) {
+
+            fruit.farmer = supplier.get();
+        } else {
+            fruit.farmer.persist();
+        }
+        fruit.persist();
     }
 }
