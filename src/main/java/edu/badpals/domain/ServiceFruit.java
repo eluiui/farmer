7905cf0a85b0affc8a7f0db84546941a156b0fdd
bestiadivina147 <diegopc147@gmail.com>
2024-06-11 13:16:19 +0200

    feat(ServiceFruit.java): add Set<Fruit> list()

--- src/main/java/edu/badpals/domain/ServiceFruit.java
+++ src/main/java/edu/badpals/domain/ServiceFruit.java
@@ -1,10 +1,17 @@
 package edu.badpals.domain;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.NoArgsConstructor;
 
 @ApplicationScoped @NoArgsConstructor
 public class ServiceFruit {
-    
+    public Set<Fruit> list() {
+        Stream<Fruit> fruits = Fruit.streamAll();
+        return fruits.collect(Collectors.toSet());
+    }
 }
